482f842151aafcc863f34ab0b46c6d7a39818e91
Eagle3 <876400276@qq.com>
2017-10-30 22:11:17 +0800

    优化

--- demo/Tmp/smarty_templates_c/3f4e6ef36b070139e65e12ac278d69065f5ee740_0.file.index.html.php
+++ demo/Tmp/smarty_templates_c/3f4e6ef36b070139e65e12ac278d69065f5ee740_0.file.index.html.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-10-30 18:44:50
+/* Smarty version 3.1.30, created on 2017-10-31 08:52:37
   from "D:\eclipse_workplace\9.1-EaglePHP\demo\App\Home\View\Index\index.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_59f702a2655e11_13590533',
+  'unifunc' => 'content_59f7c955d964c7_37792306',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -21,7 +21,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:../Common/header.html' => 1,
   ),
 ),false)) {
-function content_59f702a2655e11_13590533 (Smarty_Internal_Template $_smarty_tpl) {
+function content_59f7c955d964c7_37792306 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:../Common/header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
