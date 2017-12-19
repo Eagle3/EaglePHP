04783a706499f85fbea9c4344bc4ce5948654a54
wyk <876400276@qq.com>
2017-12-19 12:29:19 +0800

    test

--- demo/Config/dev/base.config.php
+++ demo/Config/dev/base.config.php
@@ -12,8 +12,8 @@ return array(
                 'caching' => false,              //是否使用缓存
                 'cache_lifetime' => 0, //缓存时间
                 'template_dir' => APP_PATH,//设置模板目录
-                'compile_dir' => './Tmp/smarty_templates_c',//设置编译目录
-                'cache_dir' => './Tmp/cache/smarty_templates_cache',//缓存文件夹
+                'compile_dir' => '/eagleTemp/smarty_templates_c',//设置编译目录
+                'cache_dir' => '/eagleTemp/cache/smarty_templates_cache',//缓存文件夹
                 //修改左右边界符号
                 'delimiter' => array(
                         'left_delimiter' => '{',
