0d8bffbe93ffcc8c3567ce2edf0f253406ab2fc3
wyk <876400276@qq.com>
2017-12-27 18:03:36 +0800

    test

--- Eagle/Lib/System/Download.class.php
+++ Eagle/Lib/System/Download.class.php
@@ -7,13 +7,13 @@ class Download {
     public function exe( $file ) {
         $position = strripos( $file, '.' );
         $postfix = substr( $file, $position + 1 );
-        $file = iconv( 'utf-8', 'gbk', $file ); // 兼容windows中文文件名
+        $file = iconv( 'utf-8', 'gbk', $file );
         $name = date( 'Ymdhis' ) . '.' . $postfix;
         header( "Content-type: application/octet-stream" );
         header( "Content-Type: application/force-download" );
-        header( "Content-Transfer-Encoding: binary" ); // 告诉浏览器，这是二进制文件
+        header( "Content-Transfer-Encoding: binary" );
         header( "Content-Disposition: attachment; filename=" . $name );
-        @readfile( $file );
+        readfile( $file );
         // $contents = file_get_contents($file);
         // echo $contents;
         exit();
