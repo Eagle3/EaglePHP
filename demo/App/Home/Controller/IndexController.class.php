5740de10388f6bec6c5e3098f49e9a10b66656db
wyk <876400276@qq.com>
2018-01-15 17:00:32 +0800

    test

--- demo/App/Home/Controller/IndexController.class.php
+++ demo/App/Home/Controller/IndexController.class.php
@@ -983,7 +983,59 @@ class IndexController extends CommonController {
         
     }
     
-    
+    public function hanzi2pinyin(){
+        $pinyin = new \Lib\Plugin\HanziToPinyin\src\Pinyin();
+        
+        /*
+            - 内存型，适用于服务器内存空间较富余，优点：转换快
+            - 小内存型(默认)，适用于内存比较紧张的环境，优点：占用内存小，转换不如内存型快
+            - I/O型，适用于虚拟机，内存限制比较严格环境。优点：非常微小内存消耗。缺点：转换慢，不如内存型转换快,php >= 5.5
+         */
+        
+        // 内存型
+        // $pinyin = new Pinyin('Overtrue\Pinyin\MemoryFileDictLoader');
+        // I/O型
+        // $pinyin = new Pinyin('Overtrue\Pinyin\GeneratorFileDictLoader');
+        
+        $res = $pinyin->convert('带着希望去旅行，比到达终点更美好');
+        // ["dai", "zhe", "xi", "wang", "qu", "lv", "xing", "bi", "dao", "da", "zhong", "dian", "geng", "mei", "hao"]
+        
+        $res = $pinyin->convert('带着希望去旅行，比到达终点更美好', PINYIN_UNICODE);
+        // ["dài","zhe","xī","wàng","qù","lǚ","xíng","bǐ","dào","dá","zhōng","diǎn","gèng","měi","hǎo"]
+      
+        $res = $pinyin->convert('带着希望去旅行，比到达终点更美好', PINYIN_ASCII);
+        //["dai4","zhe","xi1","wang4","qu4","lv3","xing2","bi3","dao4","da2","zhong1","dian3","geng4","mei3","hao3"]
+       
+        
+        //生成用于链接的拼音字符串
+        $res = $pinyin->permalink('带着希望去旅行'); // dai-zhe-xi-wang-qu-lv-xing
+        $res = $pinyin->permalink('带着希望去旅行', '.'); // dai.zhe.xi.wang.qu.lv.xing
+     
+        
+        //获取首字符字符串
+        $res = $pinyin->abbr('带着希望去旅行'); // dzxwqlx
+        $res = $pinyin->abbr('带着希望去旅行', '-'); // d-z-x-w-q-l-x
+        
+        /*
+                     翻译整段文字为拼音
+                     将会保留中文字符：，。 ！ ？ ： “ ” ‘ ’ 并替换为对应的英文符号。
+        */
+        
+        $res = $pinyin->sentence('带着希望去旅行，比到达终点更美好！');
+        // dai zhe xi wang qu lv xing, bi dao da zhong dian geng mei hao!
+        $res = $pinyin->sentence('带着希望去旅行，比到达终点更美好！', true);
+        // dài zhe xī wàng qù lǚ xíng, bǐ dào dá zhōng diǎn gèng měi hǎo!
+        
+        /*
+                        翻译姓名
+                        姓名的姓的读音有些与普通字不一样，比如 ‘单’ 常见的音为 dan，而作为姓的时候读 shan。
+        */
+        
+        $res = $pinyin->name('单雄信'); // ['shan', 'mou', 'mou']
+        $res = $pinyin->name('单雄信', PINYIN_UNICODE); // ["shàn","mǒu","mǒu"]
+        
+        pr( $res);
+    }
     
     
     
