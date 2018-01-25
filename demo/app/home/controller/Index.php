a4a125260ba193e54be57a0ebaa6fa1d5ba6db47
wyk <876400276@qq.com>
2018-01-25 12:05:09 +0800

    tt

--- demo/app/home/controller/Index.php
+++ demo/app/home/controller/Index.php
@@ -1301,29 +1301,44 @@ class Index extends Common {
     public function xml(){
         $o = new Xml();
         
-        /*   
+        /*    */
         $data = [
             'name' => 'jack',
             'from' => '中国',
             'data' => array(
+                    array(
+                            '广州',
+                            '南宁'
+                    ),
                     'city' => array(
                             '北京',
                             '上海'
                     )
             ),
         ];
-        $xml = $o->array2Xml($data);
+        
+        /*
+        //$xml = $o->array2Xml_DOMDocument($data);
+        
+        //$xml = $o->array2Xml_SimpleXML($data);
+        
+        $xml = $o->array2Xml_XMLWriter($data);
+        
         header( "Content-type: text/xml" );
         echo $xml;
         exit;
-       */
+      */
         
-        /* */
+        /*   */
         $xmldata = <<<xml
         <root>
           <name>jack</name>
           <from>中国</from>
           <data>
+            <item>
+                <item>广州</item>
+                <item>南宁</item>
+            </item>
             <city>
               <item>北京</item>
               <item>上海</item>
@@ -1331,7 +1346,34 @@ class Index extends Common {
           </data>
         </root>
 xml;
-        $arr = $o->xml2Array($xmldata);
+
+        $xmldata= '<xml>
+  <appid><![CDATA[wx2421b1c4370ec43b]]></appid>
+  <attach><![CDATA[支付测试]]></attach>
+  <bank_type><![CDATA[CFT]]></bank_type>
+  <fee_type><![CDATA[CNY]]></fee_type>
+  <is_subscribe><![CDATA[Y]]></is_subscribe>
+  <mch_id><![CDATA[10000100]]></mch_id>
+  <nonce_str><![CDATA[5d2b6c2a8db53831f7eda20af46e531c]]></nonce_str>
+  <openid><![CDATA[oUpF8uMEb4qRXf22hE3X68TekukE]]></openid>
+  <out_trade_no><![CDATA[1409811653]]></out_trade_no>
+  <result_code><![CDATA[SUCCESS]]></result_code>
+  <return_code><![CDATA[SUCCESS]]></return_code>
+  <sign><![CDATA[B552ED6B279343CB493C5DD0D78AB241]]></sign>
+  <sub_mch_id><![CDATA[10000100]]></sub_mch_id>
+  <time_end><![CDATA[20140903131540]]></time_end>
+  <total_fee>1</total_fee>
+<coupon_fee><![CDATA[10]]></coupon_fee>
+<coupon_count><![CDATA[1]]></coupon_count>
+<coupon_type><![CDATA[CASH]]></coupon_type>
+<coupon_id><![CDATA[10000]]></coupon_id>
+<coupon_fee><![CDATA[100]]></coupon_fee>
+  <trade_type><![CDATA[JSAPI]]></trade_type>
+  <transaction_id><![CDATA[1004400740201409030005092168]]></transaction_id>
+</xml>';
+        
+        $arr = $o->xml2Array_SimpleXML($xmldata);
+        
         pr( $arr );
         
     }
@@ -1340,8 +1382,7 @@ xml;
     
     
     
-    
-    
+        
     
     
     
--- eagle/lib/system/Xml.php
+++ eagle/lib/system/Xml.php
@@ -13,7 +13,7 @@ class Xml {
      *
      * @param array $arrayData
      *            数组
-     * @param string $parentNodeName
+     * @param string $rootNodeName
      *            根(父)节点名称
      * @param object $domDocument
      *            new \DOMDocument
@@ -21,26 +21,97 @@ class Xml {
      *            父节点对象
      * @return string XML格式字符串
      */
-    public function array2Xml( $arrayData = [], $parentNodeName = 'root', $domDocument = NULL, $parentNode = NULL ) {
+    public function array2Xml_DOMDocument( $arrayData, $rootNodeName = 'root', $domDocument = NULL, $parentNode = NULL ) {
         if ( !$domDocument ) {
             $domDocument = new \DOMDocument( '1.0', 'utf-8' );
             $domDocument->formatOutput = true;
         }
         if ( !$parentNode ) {
-            $parentNode = $domDocument->createElement( $parentNodeName );
+            $parentNode = $domDocument->createElement( $rootNodeName );
             $domDocument->appendChild( $parentNode );
         }
         foreach ( $arrayData as $key => $val ) {
-            $keyNode = $domDocument->createElement( is_string( $key ) ? $key : "item" );
+            $keyNode = $domDocument->createElement( is_string( $key ) ? $key : 'item' );
             $parentNode->appendChild( $keyNode );
             if ( !is_array( $val ) ) {
                 $valObj = $domDocument->createTextNode( $val );
                 $keyNode->appendChild( $valObj );
             } else {
-                $this->array2Xml( $val, $key, $domDocument, $keyNode );
+                $this->array2Xml_DOMDocument( $val, 'root', $domDocument, $keyNode );
             }
         }
         return $domDocument->saveXML();
+    }
+    
+    /**
+     * SimpleXML写XML - 数组转XML
+     *
+     * @param array $arrayData
+     *            数组
+     * @param string $rootNodeName
+     *            根节点名称
+     * @param object $SimpleXMLElement
+     *            new \SimpleXMLElement
+     * @param object $parentNode
+     *            父节点对象
+     * @return string XML格式字符串
+     */
+    public function array2Xml_SimpleXML( $arrayData, $rootNodeName = 'root', $SimpleXMLElement = NULL, $parentNode = NULL ) {
+        if ( !$SimpleXMLElement ) {
+            $SimpleXMLElement = new \SimpleXMLElement( "<?xml version=\"1.0\" encoding=\"utf-8\"?><{$rootNodeName}></{$rootNodeName}>" );
+        }
+        foreach ( $arrayData as $key => $val ) {
+            if ( !is_array( $val ) ) {
+                if ( is_object( $parentNode ) ) {
+                    $parentNode->addChild( is_string( $key ) ? $key : 'item', $val );
+                } else {
+                    $SimpleXMLElement->addChild( is_string( $key ) ? $key : 'item', $val );
+                }
+            } else {
+                if ( is_object( $parentNode ) ) {
+                    $keyNode = $parentNode->addChild( is_string( $key ) ? $key : 'item' );
+                } else {
+                    $keyNode = $SimpleXMLElement->addChild( is_string( $key ) ? $key : 'item' );
+                }
+                $this->array2Xml_SimpleXML( $val, '', $SimpleXMLElement, $keyNode );
+            }
+        }
+        return $SimpleXMLElement->asXml();
+    }
+    
+    /**
+     * XMLWriter写XML - 数组转XML
+     *
+     * @param array $arrayData
+     *            数组
+     * @param string $rootNodeName
+     *            根节点名称
+     * @return string XML格式字符串
+     */
+    public function array2Xml_XMLWriter( $arrayData, $rootNodeName = 'root' ) {
+        $XMLWriter = new \XMLWriter();
+        $XMLWriter->openUri( "php://output" );
+        $XMLWriter->setIndentString( "\t" );
+        $XMLWriter->setIndent( true );
+        $XMLWriter->startDocument( '1.0', 'utf-8' );
+        $XMLWriter->startElement( $rootNodeName );
+        $this->XMLWriterBuildXml( $XMLWriter, $arrayData );
+        $XMLWriter->endElement();
+        $XMLWriter->endDocument();
+        return $XMLWriter->outputMemory();
+    }
+    private function XMLWriterBuildXml( $XMLWriter, $arrayData ) {
+        foreach ( $arrayData as $key => $val ) {
+            if ( !is_array( $val ) ) {
+                $XMLWriter->startElement( is_string( $key ) ? $key : 'item' );
+                $XMLWriter->text( $val );
+                $XMLWriter->endElement();
+            } else {
+                $XMLWriter->startElement( is_string( $key ) ? $key : 'item' );
+                $this->XMLWriterBuildXml( $XMLWriter, $val );
+                $XMLWriter->endElement();
+            }
+        }
     }
     
     /**
@@ -50,7 +121,7 @@ class Xml {
      *            XML格式数据
      * @return array 数组
      */
-    public function xml2Array( $xmlData = '' ) {
+    public function xml2Array_SimpleXML( $xmlData = '' ) {
         libxml_disable_entity_loader( true );
         return json_decode( json_encode( simplexml_load_string( $xmlData, 'SimpleXMLElement', LIBXML_NOCDATA ) ), true );
     }
