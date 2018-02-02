b293685bda3f1cc7cbc364a2492c7ce7d87da333
wyk <876400276@qq.com>
2018-02-02 18:26:53 +0800

    test

--- demo/index.php
+++ demo/index.php
@@ -1,4 +1,4 @@
 <?php
 define( 'DIR', __DIR__ );
-define( 'APP_PATH', DIR . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR );
+define( 'APP_PATH', DIR . DIRECTORY_SEPARATOR . 'project' . DIRECTORY_SEPARATOR );
 require '../eagle/Eagle.php';
